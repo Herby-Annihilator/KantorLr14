2eab7b7465a2a89b71b0229234d37bc7336f14d5
Rukin Danil <rukin2018d@yandex.ru>
2021-06-23 13:16:41 +0700

    интерфейс

---
+++ Infrastructure/Commands/CloseWindowCommand.cs
@@ -0,0 +1,15 @@
+﻿using KantorLr14.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace KantorLr14.Infrastructure.Commands
+{
+	public class CloseWindowCommand : Command
+	{
+		protected override bool CanExecute(object parameter) => (parameter as Window ?? App.FocusedWindow ?? App.ActivedWindow) != null;
+
+		protected override void Execute(object parameter) => (parameter as Window ?? App.FocusedWindow ?? App.ActivedWindow)?.Close();
+	}
+}
---
+++ ViewModels/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KantorLr14.ViewModels
+{
+	public class ViewModelLocator
+	{
+		public MainWindowViewModel MainWindowModel => App.Services.GetRequiredService<MainWindowViewModel>();
+	}
+}
